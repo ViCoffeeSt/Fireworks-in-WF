86ba4a1d1bb073eb79e6b1da10897f7f98fc8ec2
ViCoffeeSt <127383715+ViCoffeeSt@users.noreply.github.com>
2023-05-05 12:19:00 +0300

    Update Form1.Designer.cs

--- Fireworks in WF/Form1.Designer.cs
+++ Fireworks in WF/Form1.Designer.cs
@@ -6,15 +6,8 @@ namespace Fireworks_in_WF
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,13 +16,9 @@ namespace Fireworks_in_WF
             }
             base.Dispose(disposing);
         }
+        
+        #region
 
-        #region Код, автоматически созданный конструктором форм Windows
-
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
@@ -38,15 +27,11 @@ namespace Fireworks_in_WF
             this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // AnimationTimer
-            // 
+
             this.AnimationTimer.Enabled = true;
             this.AnimationTimer.Interval = 20;
             this.AnimationTimer.Tick += new System.EventHandler(this.AnimationTimeEvent);
-            // 
-            // trackBar1
-            // 
+
             this.trackBar1.BackColor = System.Drawing.Color.LightGray;
             this.trackBar1.BackgroundImage = this.BackgroundImage;
             this.trackBar1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
@@ -60,9 +45,7 @@ namespace Fireworks_in_WF
             this.trackBar1.Value = 50;
             this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
             this.trackBar1.ValueChanged += new System.EventHandler(this.VolumeTrackBarChanged);
-            // 
-            // label1
-            // 
+
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(586, 407);
@@ -70,9 +53,7 @@ namespace Fireworks_in_WF
             this.label1.Size = new System.Drawing.Size(142, 31);
             this.label1.TabIndex = 1;
             this.label1.Text = "Fireworks";
-            // 
-            // Form1
-            // 
+
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
